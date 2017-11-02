e55bbfbb656b7d3962d25d152dcc6e5058fa9f66
diwen <dgm9704@hotmail.com>
2017-11-02 19:43:27 +0200

    add projects

---
+++ Diwen.FivaHeaders.Test/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace Diwen.FivaHeaders.Test
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Diwen.FivaHeaders/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Diwen.FivaHeaders
+{
+    public class Class1
+    {
+    }
+}
